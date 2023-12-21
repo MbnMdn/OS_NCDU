793b9854de37dfa42bc321698b6f86a1b84e1a4e
Erfan Golzari <bzoodee9999@gmail.com>
2023-12-21 11:01:27 +0330

    extract get directory count function

--- main.c
+++ main.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 #include <windows.h>
 
+int getNumOfDirectories(char *argv[]) {
+
+    int fileCount = 0;
+    int dirCount = 0;
+
+    WIN32_FIND_DATA file;
+    HANDLE fHandle;
+
+
+    fHandle = FindFirstFile(argv[1], &file);
+    if (fHandle == INVALID_HANDLE_VALUE) {
+        printf("Invalid File Handle.\n");
+        return 0;
+    }
+
+    while (FindNextFile(fHandle, &file) != 0) {
+        if (strcmp(file.cFileName, "..") == 0) continue;
+        switch (file.dwFileAttributes) {
+            case 16:
+                dirCount++;
+                break;
+            case 32:
+                fileCount++;
+                break;
+            default:
+                break;
+        }
+    }
+    printf("Directories: %d\nFiles: %d\n", dirCount, fileCount);
+
+}
+
+
 int main(int argc, char *argv[]) {
-    if( argc == 2 ) {
-        int fileCount = 0;
-        int dirCount = 0;
-        
-        WIN32_FIND_DATA file;
-        HANDLE fHandle;
+    if (argc == 2) {
 
-        // Place your own starting point here.
-        fHandle = FindFirstFile("D:\\spice\\*", &file);
-        if(fHandle == INVALID_HANDLE_VALUE){
-            printf("Invalid File Handle.\n");
-            return 0;
-        }
-
-        while(FindNextFile(fHandle, &file) != 0){
-            if(strcmp(file.cFileName,"..") == 0) continue;
-            switch(file.dwFileAttributes) {
-                case 16:
-                    dirCount++;
-                    break;
-                case 32:
-                    fileCount++;
-                    break;
-                default:
-                    break;
-            }
-        }
-        printf("Directories: %d\nFiles: %d\n", dirCount, fileCount);
-    }
-    else if( argc > 2 ) {
+        getNumOfDirectories(argv);
+    } else if (argc > 2) {
         printf("Too many arguments supplied.\n");
-    }
-    else {
+    } else {
         printf("One argument expected.\n");
     }
 }
