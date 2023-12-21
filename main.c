f9598b4095bfc32c935501b5e2a64602219e1f82
Erfan Golzari <bzoodee9999@gmail.com>
2023-12-21 10:34:58 +0330

    get directory and file count

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
+#include <windows.h>
 
 int main(int argc, char *argv[]) {
     if( argc == 2 ) {
-        printf("The argument supplied is %s\n", argv[1]);
+        int fileCount = 0;
+        int dirCount = 0;
+        
+        WIN32_FIND_DATA file;
+        HANDLE fHandle;
+
+        // Place your own starting point here.
+        fHandle = FindFirstFile("D:\\spice\\*", &file);
+        if(fHandle == INVALID_HANDLE_VALUE){
+            printf("Invalid File Handle.\n");
+            return 0;
+        }
+
+        while(FindNextFile(fHandle, &file) != 0){
+            if(strcmp(file.cFileName,"..") == 0) continue;
+            switch(file.dwFileAttributes) {
+                case 16:
+                    dirCount++;
+                    break;
+                case 32:
+                    fileCount++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        printf("Directories: %d\nFiles: %d\n", dirCount, fileCount);
     }
     else if( argc > 2 ) {
         printf("Too many arguments supplied.\n");
